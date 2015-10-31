a28f72ec183dbbb50eb91dfe17426fb2d689b545
Michael Chekin <mchekin@gmail.com>
2015-10-31 21:37:27 +0200

    Added default port 3306 to Mysql database params
    This is to fix connection to Mysql inside the Homestead virtual machine ( port 33060 should be used in .env file in the host developing machine)

--- config/database.php
+++ config/database.php
@@ -62,6 +62,7 @@ return [
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
             'strict'    => false,
+            'port'      => env('DB_PORT', '3306'),
         ],
 
         'pgsql' => [
