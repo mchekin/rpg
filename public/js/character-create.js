8c2d3a323cc9ffdef99f2c5bb5ff5d84165bfa7c
Michael Chekin <mchekin@gmail.com>
2015-11-17 20:58:40 +0200

    Small styling edit

--- public/js/character-create.js
+++ public/js/character-create.js
@@ -1,10 +1,7 @@
-
-    console.log( "ready!" );
-
-    // Enable Carousel Controls
-    $(".left").click(function(){
-        $(".carousel").carousel("prev");
-    });
-    $(".right").click(function(){
-        $(".carousel").carousel("next");
-    });
+// Enable Carousel Controls
+$(".left").click(function(){
+    $(".carousel").carousel("prev");
+});
+$(".right").click(function(){
+    $(".carousel").carousel("next");
+});
