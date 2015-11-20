73a348e632dadc12a57b189efe15de4b4bec52b3
Michael Chekin <mchekin@gmail.com>
2015-11-20 16:41:14 +0200

    added 'font-awesome' glyphicons css cdn link

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -5,6 +5,7 @@
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
             <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         @show
     </head>
