519f3669d65295a3fa06c6da8199f0f272ee0a68
Michael <mchekin@gmail.com>
2015-12-09 21:08:27 +0200

    Updated temporal Character info display in 'home' view

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -10,15 +10,49 @@
 @section("body")
     <div class="content col-md-8 col-md-offset-2">
         <h2>Here is your character:</h2>
-        <dl>
+        <hr>
+        <dl class="dl-horizontal">
+
             <dt>Name</dt>
             <dd>{{ $character->name }}</dd>
+
+            <dt>Gender</dt>
+            <dd>{{ $character->gender }}</dd>
+
+            <dt>XP</dt>
+            <dd>{{ $character->xp }}</dd>
+
+            <dt>Level</dt>
+            <dd>{{ $character->level }}</dd>
+
+            <dt>Gold</dt>
+            <dd>{{ $character->money }}</dd>
 
             <dt>Race</dt>
             <dd>{{ $character->race->name }}</dd>
 
             <dt>Location</dt>
             <dd>{{ $character->location->name }}</dd>
+
         </dl>
+        <hr>
+        <dl class="dl-horizontal">
+
+            <dt>Strength</dt>
+            <dd>{{ $character->strength }}</dd>
+
+            <dt>Agility</dt>
+            <dd>{{ $character->agility }}</dd>
+
+            <dt>Constitution</dt>
+            <dd>{{ $character->constitution }}</dd>
+
+            <dt>Intelligence</dt>
+            <dd>{{ $character->intelligence }}</dd>
+
+            <dt>Charisma</dt>
+            <dd>{{ $character->charisma }}</dd>
+        </dl>
+        <hr>
     </div>
 @stop
