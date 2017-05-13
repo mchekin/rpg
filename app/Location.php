7d97685790bb40c9c3277c438c5a305f957a27fe
Michael Chekin <mchekin@gmail.com>
2017-05-13 22:20:05 +0300

    fixing adjacent location record creation for SQLite ( by adding created_at and updated_at values manually)

--- app/Location.php
+++ app/Location.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
@@ -74,8 +75,17 @@ class Location extends Model
             throw new \InvalidArgumentException('Invalid adjacent direction type: '.$direction);
         }
 
-        $this->adjacentLocations()->attach($adjacent, ['direction' => $direction]); // add adjacent
-        $adjacent->adjacentLocations()->attach($this, ['direction' => self::getAppositeDirection($direction)]); // add yourself, too
+        $this->adjacentLocations()->attach($adjacent, [
+            'direction' => $direction,
+            "created_at"            => Carbon::now(),
+            "updated_at"            => Carbon::now(),
+        ]); // add adjacent
+
+        $adjacent->adjacentLocations()->attach($this, [
+            'direction' => self::getAppositeDirection($direction),
+            "created_at"            => Carbon::now(),
+            "updated_at"            => Carbon::now(),
+        ]); // add yourself, too
     }
 
     /**
