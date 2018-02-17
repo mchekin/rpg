40c612e07264407659ac2827a9cb7c184ece33c4
Michael Chekin <mchekin@gmail.com>
2018-02-17 08:32:17 +0100

    adding 2 simple Character attack tests (#10)
    Two simple tests for the attacking mechanism:
    
    Testing that a strong own character defeats a basic defender, while attacking it.
    Testing that a basic own character loses to a strong defender, while attacking it.

---
+++ tests/Feature/AttackTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Battle;
+use App\Character;
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class AttackTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * @test
+     */
+    public function strong_own_character_wins_while_attacking_basic_opponent()
+    {
+        // Given we have a signed in user that already has a strongCharacter
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+        /** @var Character $strongOwnCharacter */
+        $strongOwnCharacter = factory(Character::class)->create([
+            'user_id' => $user,
+            'hit_points' => 1000,
+            'strength' => 1000,
+            'agility' => 1000,
+            'constitution' => 1000,
+            'intelligence' => 1000,
+            'charisma' => 1000,
+        ]);
+        $basicOpponentCharacter = factory(Character::class)->create([
+            'location_id' => $strongOwnCharacter->location_id
+        ]);
+
+        // When we hit home folder
+        $response = $this->get("/character/{$basicOpponentCharacter->id}/attack");
+
+        $battle = Battle::query()->firstOrFail();
+        // We are redirected to the Battle page
+        $response->assertRedirect(route('battle.show', $battle->id));
+        $this->assertEquals($strongOwnCharacter->id, $battle->attacker_id);
+        $this->assertEquals($strongOwnCharacter->id, $battle->victor_id);
+        $this->assertEquals($basicOpponentCharacter->id, $battle->defender_id);
+    }
+
+    /**
+     * @test
+     */
+    public function basic_own_character_looses_while_attacking_strong_opponent()
+    {
+        // Given we have a signed in user that already has a strongCharacter
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+        /** @var Character $basicOwnCharacter */
+        $basicOwnCharacter = factory(Character::class)->create([
+            'user_id' => $user,
+        ]);
+        $strongOpponentCharacter = factory(Character::class)->create([
+            'location_id' => $basicOwnCharacter->location_id,
+            'hit_points' => 1000,
+            'strength' => 1000,
+            'agility' => 1000,
+            'constitution' => 1000,
+            'intelligence' => 1000,
+            'charisma' => 1000,
+        ]);
+
+        // When we hit home folder
+        $response = $this->get("/character/{$strongOpponentCharacter->id}/attack");
+
+        $battle = Battle::query()->firstOrFail();
+        // We are redirected to the Battle page
+        $response->assertRedirect(route('battle.show', $battle->id));
+        $this->assertEquals($basicOwnCharacter->id, $battle->attacker_id);
+        $this->assertEquals($strongOpponentCharacter->id, $battle->victor_id);
+        $this->assertEquals($strongOpponentCharacter->id, $battle->defender_id);
+    }
+}
