5183e82d6ffdb89f02fc0c230dd07b1f1dd5bb48
Michael Chekin <mchekin@gmail.com>
2018-02-17 21:47:50 +0100

    some commented code back to the original version

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')
-        //    ->everyMinute();
+        //    ->hourly();
     }
 
     /**
