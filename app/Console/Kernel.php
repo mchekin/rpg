62c5d21b62f8490645efbf4e69daa046cd5ea9d6
Michael Chekin <mchekin@gmail.com>
2018-02-17 16:47:39 +0100

    removing debugging character seeding

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,7 +2,6 @@
 
 namespace App\Console;
 
-use App\Character;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -25,12 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('inspire')
-                  ->everyMinute();
-
-        $schedule->call(function () {
-            factory(Character::class, 1)->create();
-        })->everyMinute();
+//        $schedule->command('inspire')
+//                  ->everyMinute();
     }
 
     /**
