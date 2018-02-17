a38dfa859f18971a2c74b03d796b44541801238c
Michael Chekin <mchekin@gmail.com>
2018-02-17 21:46:54 +0100

    commented text indentation

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-//        $schedule->command('inspire')
-//                  ->everyMinute();
+        // $schedule->command('inspire')
+        //    ->everyMinute();
     }
 
     /**
