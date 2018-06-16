89da884c096bf03b933aeb34faccec9349af12ec
Michael Chekin <mchekin@gmail.com>
2018-06-16 07:05:18 +0200

    Minor message container refactoring

--- resources/views/message/partials/my-message.blade.php
+++ resources/views/message/partials/my-message.blade.php
@@ -1,5 +1,11 @@
-<div class="message-list-container">
-    <img src="https://vignette.wikia.nocookie.net/forgottenrealms/images/f/fa/Jon_Irenicus.jpg" alt="Avatar">
-    <p>{!! $message->content !!}</p>
-    <span class="message-list-time-right">{{ $message->created_at }}</span>
+<div class="message-list-container row">
+    <div class="col-sm-2 text-center">
+        <div class="clearfix">
+            <img src="https://vignette.wikia.nocookie.net/forgottenrealms/images/f/fa/Jon_Irenicus.jpg" alt="Avatar">
+        </div>
+        <div>{{ $message->created_at }}</div>
+    </div>
+    <div class="col-sm-10">
+        <p>{!! $message->content !!}</p>
+    </div>
 </div>
--- resources/views/message/partials/others-message.blade.php
+++ resources/views/message/partials/others-message.blade.php
@@ -1,5 +1,11 @@
 <div class="message-list-container message-list-darker">
-    <img src="https://vignette.wikia.nocookie.net/forgottenrealms/images/9/95/Sarevok_-_Throne_of_Bhaal.png" alt="Avatar" class="right">
-    <p>{!! $message->content !!}</p>
-    <span class="message-list-time-left">{{ $message->created_at }}</span>
+    <div class="col-sm-10">
+        <p>{!! $message->content !!}</p>
+    </div>
+    <div class="col-sm-2 text-center">
+        <div class="clearfix">
+            <img src="https://vignette.wikia.nocookie.net/forgottenrealms/images/9/95/Sarevok_-_Throne_of_Bhaal.png" alt="Avatar" class="right">
+        </div>
+        <div>{{ $message->created_at }}</div>
+    </div>
 </div>
