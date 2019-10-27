621ac9b26be243c901859ecb2debfa9ed056e61b
Michael Chekin <mchekin@gmail.com>
2019-10-27 20:02:32 +0100

    Converting location navigation to POST

--- resources/views/location/partials/navigator.blade.php
+++ resources/views/location/partials/navigator.blade.php
@@ -1,59 +1,81 @@
-<div class="row location-navigator mt-5 mb-5">
-    <div class="col">
-        <div class="row">
-            <div class="col"></div>
-            <div class="col text-center my-auto">
-                @if(!is_null($adjacent = $location->adjacent('north')))
-                    <div>
-                        <a href="{{ route('character.move', ['character' => Auth::user()->character, 'location' => $location->adjacent('north')]) }}">
-                            {{ $location->adjacent('north')->name }}
-                        </a>
-                    </div>
-                    <span class="fa fa-angle-up"></span>
-                @endif
-            </div>
-            <div class="col"></div>
-        </div>
+@php
+    /** @var \App\Character $character */
+    $character = Auth::user()->character;
+@endphp
 
-        <div class="row">
-            <div class="col text-right my-auto">
-                @if(!is_null($adjacent = $location->adjacent('west')))
-                    <div>
-                        <a href="{{ route('character.move', ['character' => Auth::user()->character, 'location' => $location->adjacent('west')]) }}">
-                            {{ $location->adjacent('west')->name }}
-                        </a>
-                        <span class="fa fa-angle-left"></span>
-                    </div>
-                @endif
+<form role="form" method="POST" class="w-100">
+    {!! csrf_field() !!}
+    <div class="row location-navigator mt-5 mb-5">
+        <div class="col">
+            <div class="row">
+                <div class="col"></div>
+                <div class="col text-center my-auto">
+                    @if(!is_null($adjacent = $location->adjacent('north')))
+                        <div>
+                            <button type="submit"
+                                    class="btn btn-link"
+                                    formaction="{{ route('character.move', ['character' => $character, 'location' => $location->adjacent('north')]) }}">
+                                {{ $location->adjacent('north')->name }}
+                            </button>
+                        </div>
+                        <span class="fa fa-angle-up"></span>
+                    @endif
+                </div>
+                <div class="col"></div>
             </div>
-            <div class="col text-center my-auto"><span class="fas fa-walking fa-2x"></span></div>
-            <div class="col text-left my-auto">
-                @if(!is_null($adjacent = $location->adjacent('east')))
-                    <div>
-                        <span class="fa fa-angle-right"></span>
-                        <a href="{{ route('character.move', ['character' => Auth::user()->character, 'location' => $location->adjacent('east')]) }}">
-                            {{ $location->adjacent('east')->name }}
-                        </a>
-                    </div>
-                @endif
-            </div>
-        </div>
 
-        <div class="row">
-            <div class="col"></div>
-            <div class="col text-center my-auto">
-                @if(!is_null($adjacent = $location->adjacent('south')))
-                    <span class="fa fa-angle-down"></span>
-                    <div>
-                        <a href="{{ route('character.move', ['character' => Auth::user()->character, 'location' => $location->adjacent('south')]) }}">
-                            {{ $location->adjacent('south')->name }}
-                        </a>
-                    </div>
-                @endif
+            <div class="row">
             </div>
-            <div class="col"></div>
+
+            <div class="row">
+                <div class="col text-right my-auto">
+                    @if(!is_null($adjacent = $location->adjacent('west')))
+                        <div>
+                            <button type="submit"
+                                    class="btn btn-link"
+                                    formaction="{{ route('character.move', ['character' => $character, 'location' => $location->adjacent('west')]) }}">
+                                {{ $location->adjacent('west')->name }}
+                            </button>
+                            <span class="fa fa-angle-left"></span>
+                        </div>
+                    @endif
+                </div>
+                <div class="col text-center my-auto"><span class="fas fa-walking fa-2x"></span></div>
+                <div class="col text-left my-auto">
+                    @if(!is_null($adjacent = $location->adjacent('east')))
+                        <div>
+                            <span class="fa fa-angle-right"></span>
+                            <button type="submit"
+                                    class="btn btn-link"
+                                    formaction="{{ route('character.move', ['character' => $character, 'location' => $location->adjacent('east')]) }}">
+                                {{ $location->adjacent('east')->name }}
+                            </button>
+                        </div>
+                    @endif
+                </div>
+            </div>
+
+            <div class="row">
+            </div>
+
+            <div class="row">
+                <div class="col"></div>
+                <div class="col text-center my-auto">
+                    @if(!is_null($adjacent = $location->adjacent('south')))
+                        <span class="fa fa-angle-down"></span>
+                        <div>
+                            <button type="submit"
+                                    class="btn btn-link"
+                                    formaction="{{ route('character.move', ['character' => $character, 'location' => $location->adjacent('south')]) }}">
+                                {{ $location->adjacent('south')->name }}
+                            </button>
+                        </div>
+                    @endif
+                </div>
+                <div class="col"></div>
+            </div>
+
         </div>
 
     </div>
-
-</div>
+</form>
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use App\Message;
 */
 
 // Route models...
-Route::get('/character/{character}/location/{location}/move', 'CharacterController@getMove')->name('character.move');
+Route::post('/character/{character}/location/{location}/move', 'CharacterController@getMove')->name('character.move');
 Route::get('/character/{character}/attack', 'CharacterController@getAttack')->name('character.attack');
 Route::post('/inventory/item/{item}/equip', 'InventoryController@equipItem')->name('inventory.item.equip');
 Route::post('/inventory/item/{item}/un-equip', 'InventoryController@unEquipItem')->name('inventory.item.un-equip');
