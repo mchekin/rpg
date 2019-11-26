5ebd194a2a5fc64c201cbf5c9f6c866151153a0a
Michael Chekin <mchekin@gmail.com>
2019-11-26 23:10:47 +0100

    Fixing Voyager migrations.

--- config/voyager.php
+++ config/voyager.php
@@ -87,7 +87,7 @@ return [
         'tables' => [
             'hidden' => ['migrations', 'data_rows', 'data_types', 'menu_items', 'password_resets', 'permission_role', 'settings'],
         ],
-        'autoload_migrations' => true,
+        'autoload_migrations' => false,
     ],
 
     /*
---
+++ database/migrations/2017_11_26_013050_add_user_role_relationship.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class AddUserRoleRelationship extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->bigInteger('role_id')->unsigned()->change();
+            $table->foreign('role_id')->references('id')->on('roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            if (DB::getDriverName() !== 'sqlite') {
+                $table->dropForeign(['role_id']);
+            }
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->bigInteger('role_id')->change();
+        });
+    }
+}
