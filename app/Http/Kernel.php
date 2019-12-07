727755e1081370f531a4d7580a6cd436c0305ae2
Michael Chekin <mchekin@gmail.com>
2019-12-07 13:04:28 +0100

    Updating project files for Laravel 6.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,7 +57,10 @@ class Kernel extends HttpKernel
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
+        'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'character.location' => \App\Http\Middleware\IsCharacterLocation::class,
         'has.character' => \App\Http\Middleware\HasCharacter::class,
         'can.move.to.location' => \App\Http\Middleware\CanMoveToLocation::class,
@@ -65,5 +68,22 @@ class Kernel extends HttpKernel
         'no.character' => \App\Http\Middleware\NoCharacterYet::class,
         'owns.character' => \App\Http\Middleware\UserOwnsCharacter::class,
         'is.admin' => \App\Http\Middleware\IsAdmin::class,
+    ];
+
+    /**
+     * The priority-sorted list of middleware.
+     *
+     * This forces non-global middleware to always be in the given order.
+     *
+     * @var array
+     */
+    protected $middlewarePriority = [
+        \Illuminate\Session\Middleware\StartSession::class,
+        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        \App\Http\Middleware\Authenticate::class,
+        \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        \Illuminate\Session\Middleware\AuthenticateSession::class,
+        \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        \Illuminate\Auth\Middleware\Authorize::class,
     ];
 }
--- config/logging.php
+++ config/logging.php
@@ -1,5 +1,6 @@
 <?php
 
+use Monolog\Handler\NullHandler;
 use Monolog\Handler\StreamHandler;
 
 return [
@@ -75,6 +76,11 @@ return [
         'errorlog' => [
             'driver' => 'errorlog',
             'level' => 'debug',
+        ],
+
+        'null' => [
+            'driver' => 'monolog',
+            'handler' => NullHandler::class,
         ],
     ],
 
