709e4c07c0600cdf9a0178c77aeac44d0f1b03c2
Michael Chekin <mchekin@gmail.com>
2019-12-22 11:06:07 +0100

    Adding more entity mappings.

--- app/Modules/Character/Domain/ValueObjects/Inventory.php
+++ app/Modules/Character/Domain/ValueObjects/Inventory.php
@@ -9,28 +9,28 @@ use App\Modules\Character\Domain\ValueObjects\Inventory\NotEnoughSpaceException;
 use App\Modules\Equipment\Domain\Entities\Item;
 use App\Modules\Equipment\Domain\ValueObjects\InventorySlot;
 use App\Modules\Equipment\Domain\ValueObjects\ItemType;
-use Illuminate\Support\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 
 class Inventory
 {
     const NUMBER_OF_SLOTS = 24;
 
     /**
-     * @var Collection
+     * @var ArrayCollection
      */
     private $items;
 
-    private function __construct(Collection $items)
+    private function __construct(ArrayCollection $items)
     {
-        $this->items = $items;
+        $this->items = new ArrayCollection($items->toArray());
     }
 
     public static function empty(): self
     {
-        return new self(new Collection());
+        return new self(new ArrayCollection());
     }
 
-    public static function withItems(Collection $items): self
+    public static function withItems(ArrayCollection $items): self
     {
         if ($items->count() >= self::NUMBER_OF_SLOTS) {
             throw new NotEnoughSpaceException("Not enough space in the Inventory for {$items->count()} new items");
@@ -45,28 +45,24 @@ class Inventory
             throw new InventorySlotOutOfRangeException("Inventory slot $slot is out of range.");
         }
 
-        if ($this->items->has($slot)) {
+        if ($this->items->contains($slot)) {
             throw new InventorySlotIsTakenException("Inventory slot $slot is already take");
         }
 
-        $item->setInventorySlot(InventorySlot::defined($slot));
-
-        return new self($this->items->put($slot, $item));
+        return $this->addItem($slot, $item);
     }
 
-    public function withAddedItemToFreeSlot(Item $item)
+    public function withAddedItemToFreeSlot(Item $item): self
     {
         $slot = $this->findFreeSlot();
 
-        $item->setInventorySlot(InventorySlot::defined($slot));
-
-        return new self($this->items->put($slot, $item));
+        return $this->addItem($slot, $item);
     }
 
     public function findFreeSlot(): int
     {
         for ($slot = 0; $slot < self::NUMBER_OF_SLOTS; $slot++) {
-            if (!$this->items->has($slot)) {
+            if (!$this->items->contains($slot)) {
                 return $slot;
             }
         }
@@ -74,14 +70,14 @@ class Inventory
         throw new InventoryIsFullException('Cannot add to full inventory');
     }
 
-    public function findEquippedItemOfType(ItemType $type)
+    public function findEquippedItemOfType(ItemType $type): Item
     {
-        return $this->items->first(function (Item $item) use ($type) {
+        return $this->items->filter(function (Item $item) use ($type) {
             return $item->getType()->equals($type) && $item->isEquipped();
-        });
+        })->first();
     }
 
-    public function hasItem(Item $itemToFind)
+    public function hasItem(Item $itemToFind): bool
     {
         return $this->items->contains(function (Item $item) use ($itemToFind) {
             return $item->getId() === $itemToFind->getId();
@@ -90,7 +86,7 @@ class Inventory
 
     public function getEquippedItemsEffect(string $itemEffectType): int
     {
-        return (int)$this->getEquippedItems()->reduce(function ($carry, Item $item) use ($itemEffectType) {
+        return (int)array_reduce($this->getEquippedItems()->toArray(), function ($carry, Item $item) use ($itemEffectType) {
 
             $itemEffect = $item->getItemEffect($itemEffectType);
 
@@ -98,10 +94,25 @@ class Inventory
         });
     }
 
-    private function getEquippedItems(): Collection
+    private function getEquippedItems(): ArrayCollection
     {
         return $this->items->filter(function (Item $item) {
             return $item->isEquipped();
         });
+    }
+
+    /**
+     * @param int $slot
+     * @param Item $item
+     * @return Inventory
+     */
+    private function addItem(int $slot, Item $item): Inventory
+    {
+        $item->setInventorySlot(InventorySlot::defined($slot));
+
+        $items = $this->items;
+        $items->set($slot, $item);
+
+        return new self($this->items);
     }
 }
