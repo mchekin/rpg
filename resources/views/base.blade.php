2ca29ae8818ae7516046ad27f413d238f77a7a2e
Michael Chekin <mchekin@gmail.com>
2020-05-24 22:36:03 +0200

    Generating js and css files using npm.

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -1,19 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    @section("head")
+    @section('head')
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="google" content="notranslate">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         {{--<meta http-equiv="refresh" content="30">--}}
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
-              integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
-              crossorigin="anonymous">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <link rel="stylesheet"
               href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
               integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
               crossorigin="anonymous">
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @show
 </head>
 <body>
@@ -25,20 +23,10 @@
 
     @include('partials.status')
 
-    @yield("body")
+    @yield('body')
 </div>
 
-@section("footer")
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js"
-            integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT"
-            crossorigin="anonymous">
-    </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-            integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-            crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"
-            integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
-            crossorigin="anonymous"></script>
+@section('footer')
     <script src="{{ asset('js/app.js') }}"></script>
 @show
 </body>
