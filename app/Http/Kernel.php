127aa40708cda2fa064b4b3973dc3eea89523b26
Michael Chekin <mchekin@gmail.com>
2020-07-16 19:13:36 +0200

    Revert "Move Store managements api routes to api.php + using the same middleware in the api middleware group"
    This reverts commit 9ef51afa86a5e6a822d4dfec45bef062e86607b5.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,14 +40,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             'throttle:60,1',
-            \App\Http\Middleware\EncryptCookies::class,
-            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
-            // \Illuminate\Session\Middleware\AuthenticateSession::class,
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\UpdateLastUserActivity::class,
+            'bindings',
         ],
     ];
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\Request;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -47,4 +49,13 @@ Route::resource('character.battle', 'CharacterBattleController')->only(['index']
 
 Route::group(['prefix' => 'admin'], static function () {
     Voyager::routes();
+});
+
+Route::middleware('auth', 'has.character')->namespace('Api')->prefix('api')->group(static function () {
+
+    Route::post('/inventory/item/{item}/move-to-store', 'StoreController@moveItemToStore')->name('inventory.item.move-to-store');
+    Route::post('/store/item/{item}/move-to-inventory', 'StoreController@moveItemToInventory')->name('store.item.move-to-inventory');
+    Route::post('/inventory/money/move-to-store', 'StoreController@moveMoneyToStore')->name('inventory.money.move-to-store');
+    Route::post('/store/money/move-to-inventory', 'StoreController@moveMoneyToInventory')->name('store.money.move-to-inventory');
+
 });
