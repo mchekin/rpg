5b66e9263395e0431ae48640aa21dac1dd3945a3
Michael Chekin <mchekin@gmail.com>
2021-03-21 15:48:43 +0100

    Buy From Character Store: Making sure ErrorMessages message appears after dismissal.
    Signed-off-by: Michael Chekin <mchekin@gmail.com>

--- resources/js/components/ErrorMessages.vue
+++ resources/js/components/ErrorMessages.vue
@@ -1,28 +1,36 @@
 <template>
-  <div v-if="errors.length" id="errors-alert" class="alert alert-danger alert-dismissible fade show" role="alert">
-    <ul class="list-unstyled">
-      <li v-for="error in errors" class="">
-        {{ error }}
-      </li>
-    </ul>
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-      <span aria-hidden="true">&times;</span>
-    </button>
-  </div>
+  <Transition name="slide-fade">
+    <div v-if="errors.length" id="errors-alert" class="alert alert-danger alert-dismissible" role="alert">
+      <ul class="list-unstyled">
+        <li v-for="error in errors" class="">
+          {{ error }}
+        </li>
+      </ul>
+      <button type="button" class="close" aria-label="Close" v-on:click="errors = []">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+  </Transition>
 </template>
 
 <script>
 export default {
   mounted() {
+    let timer;
     this.$root.$on('errorHappened', error => {
-      this.errors = [error];
+      clearTimeout(timer);
+
+      this.errors.push(error);
+
+      timer = setTimeout(() => {
+        this.errors = [];
+      }, 10000);
     });
   },
 
   data() {
     return {
-      errors: [
-      ]
+      errors: []
     };
   },
 
@@ -30,8 +38,18 @@ export default {
     this.errors = this.$attrs.errors;
   },
 
-  methods: {
-
-  }
+  methods: {}
 }
 </script>
+<style scoped>
+.slide-fade-enter-active,
+.slide-fade-leave-active {
+  transition: all 0.4s;
+}
+
+.slide-fade-enter,
+.slide-fade-leave-to {
+  transform: translateX(400px);
+  opacity: 0;
+}
+</style>
--- resources/js/notifications.js
+++ resources/js/notifications.js
@@ -1,7 +1,4 @@
 (function($) {
-    $("#errors-alert").fadeTo(15000, 500).slideUp(500, function(){
-        $("#errors-alert").slideUp(500);
-    });
     $("#status-alert").fadeTo(2000, 500).slideUp(500, function(){
         $("#errors-alert").slideUp(500);
     });
