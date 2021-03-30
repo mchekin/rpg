c59624e28d5f82435abb05b806d9c8da3613c962
Michael Chekin <mchekin@gmail.com>
2021-03-30 13:22:02 +0200

    Buy From Character Store: Removing extra instructions from PopupModal.vue
    Signed-off-by: Michael Chekin <mchekin@gmail.com>

--- resources/js/components/PopupModal.vue
+++ resources/js/components/PopupModal.vue
@@ -56,15 +56,6 @@ export default {
   font-family: Helvetica, Arial, sans-serif;
 }
 
-/*
- * The following styles are auto-applied to elements with
- * transition="modal" when their visibility is toggled
- * by Vue.js.
- *
- * You can easily play with the modal transition by editing
- * these styles.
- */
-
 .modal-enter {
   opacity: 0;
 }
