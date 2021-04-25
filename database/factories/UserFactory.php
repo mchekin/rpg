91c8c9320bfc0d1b1d32fed49fe1b77ada3d94f9
Michael Chekin <mchekin@gmail.com>
2021-04-25 19:48:23 +0200

    Upgrade to Laravel 8: updating UserFactory.php and User migration

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,15 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| Here you may define all of your model factories. Model factories give
-| you a convenient way to create models for testing and seeding your
-| database. Just tell the factory how a default model should look.
-|
-*/
+namespace Database\Factories;
 
 use App\Character;
 use App\Inventory;
@@ -38,6 +29,7 @@ $factory->define(User::class, static function (Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
+        'email_verified_at' => now(),
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => Str::random(60),
         'api_token' => Str::random(60),
