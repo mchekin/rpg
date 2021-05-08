b2fabecdc2bd8e4d8002689f2c3ff60568aaf816
Michael Chekin <mchekin@gmail.com>
2021-05-08 12:10:05 +0200

    To-Laravel-8: fixing building Front-end packages 2

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,5 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .sass('resources/sass/app.scss', 'public/css')
+    .vue();
